902ed761858ebc1b9ece96727b317f636f26c261
Scott Murphy <scott@alwaysvip.com>
2019-06-01 10:45:46 -0700

    Create Person in Bootstrap.groovy

--- grails-app/init/gormless/BootStrap.groovy
+++ grails-app/init/gormless/BootStrap.groovy
@@ -2,7 +2,10 @@ package gormless
 
 class BootStrap {
 
+	def personService
+
     def init = { servletContext ->
+    	personService.save(new Person(name:'Graeme Rocher'))
     }
     def destroy = {
     }
